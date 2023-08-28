0121e4acc071a7b74961e4210bca9155e2038781
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2023-08-28 16:46:52 +0800

    chore: popular products

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
         <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
           {statistics.map((stat) => (
             <div key={stat.label}>
-              <p className='text-4xl font-palaquin font-bold'>{stat.value}</p>
+              <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
               <p className='leading-7 font-montserrat text-slate-gray'>{stat.label}</p>
             </div>
           ))}
