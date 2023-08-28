a66468ce59516bfe259a3317d1f764229a21aff0
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2023-08-28 14:23:29 +0800

    chore: folder structure

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Nav from "./Nav";
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+    Nav,
+    Hero,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
